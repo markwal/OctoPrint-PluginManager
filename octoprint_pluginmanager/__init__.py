a2d0d9642680aea6e5a12d2ad11b10bc5c4005d9
Gina Häußge <osd@foosel.net>
2015-01-26 22:30:49 +0100

    WIP: installing and uninstalling works now

--- octoprint_pluginmanager/__init__.py
+++ octoprint_pluginmanager/__init__.py
@@ -14,6 +14,7 @@ __plugin_url__ = "http://octoprint.org"
 
 import octoprint.plugin
 
+from octoprint.settings import valid_boolean_trues
 from flask import jsonify, make_response
 
 default_settings = dict()
@@ -44,48 +45,106 @@ class PluginManagerPlugin(octoprint.plugin.SimpleApiPlugin,
 	def get_api_commands(self):
 		return {
 			"install": ["url"],
-			"uninstall": ["plugin"]
+			"uninstall": ["plugin"],
+			"toggle": ["plugin"]
 		}
 
 	def on_api_get(self, request):
 		plugins = self._plugin_manager.plugins
+		disabled_plugins = self._plugin_manager.disabled_plugins
 
 		result = []
-		for name, plugin in plugins.items():
-			result.append(dict(key=name, name=plugin.name, description=plugin.description, author=plugin.author, version=plugin.version, url=plugin.url))
+		for d in (plugins, disabled_plugins):
+			for name, plugin in d.items():
+				result.append(dict(
+					key=name,
+					name=plugin.name,
+					description=plugin.description,
+					author=plugin.author,
+					version=plugin.version,
+					url=plugin.url,
+					license=plugin.license,
+					bundled=plugin.bundled,
+					enabled=plugin.enabled
+				))
 
 		return jsonify(plugins=result)
 
 	def on_api_command(self, command, data):
+		if command == "install":
+			url = data["url"]
+			return self.command_install(url, force="force" in data and data["force"] in valid_boolean_trues)
+
+		elif command == "uninstall":
+			plugin_name = data["plugin"]
+			if not plugin_name in self._plugin_manager.plugins:
+				return make_response("Unknown plugin: %s" % plugin_name, 404)
+
+			plugin = self._plugin_manager.plugins[plugin_name]
+			return self.command_uninstall(plugin)
+
+		elif command == "toggle":
+			plugin_name = data["plugin"]
+			if not plugin_name in self._plugin_manager.plugins:
+				return make_response("Unknown plugin: %s" % plugin_name, 404)
+
+			plugin = self._plugin_manager.plugins[plugin_name]
+			return self.command_toggle(plugin)
+
+	def command_install(self, url, force=False):
 		try:
 			import pip as _pip
 		except:
 			return make_response("Could not instantiate pip, can't install that, sorry", 500)
 
-		if command == "uninstall":
-			plugin_name = data["plugin"]
-			if not plugin_name in self._plugin_manager.plugins:
-				return make_response("Unknown plugin: %s" % plugin_name, 404)
-			plugin = self._plugin_manager.plugins[plugin_name]
-			origin_type, origin_location = plugin.origin
+		pip_args = ["install", url]
+		try:
+			_pip.main(pip_args)
+		except:
+			self._logger.exception("Could not install plugin from %s" % url)
+			return make_response("Could not install plugin from url, see the log for more details", 500)
+		else:
+			if force:
+				pip_args += ["--ignore-installed", "--force-reinstall", "--no-deps"]
+				try:
+					_pip.main(pip_args)
+				except:
+					self._logger.exception("Could not install plugin from %s" % url)
+					return make_response("Could not install plugin from url, see the log for more details", 500)
 
-			if origin_type == "entry_point":
-				# plugin is installed through entry point, need to use pip to uninstall it
-				pip_args = ["uninstall", plugin.identifier]
-				_pip.main(pip_args)
+	def command_uninstall(self, plugin):
+		if plugin.key == "pluginmanager":
+			return make_response("Can't uninstall Plugin Manager", 400)
 
-			elif origin_type == "folder":
-				# plugin is installed via a plugin folder, need to use rmtree to get rid of it
+		try:
+			import pip as _pip
+		except:
+			return make_response("Could not instantiate pip, can't install that, sorry", 500)
 
-				# first make sure that the plugin folder is our custom one!
-				global s
-				if not origin_location.startswith(s.globalGetBaseFolder("plugins")):
-					return make_response("Only user installed plugins are allowed to be uninstalled", 400)
+		if plugin.bundled:
+			return make_response("Bundled plugins cannot be uninstalled", 400)
 
-				# all clear, remove it
-				import shutil
-				import os
-				shutil.rmtree(os.path.realpath(plugin.location))
+		if plugin.origin[0] == "entry_point":
+			# plugin is installed through entry point, need to use pip to uninstall it
+			pip_args = ["uninstall", "--yes", plugin.origin[2]]
+			_pip.main(pip_args)
+
+		elif plugin.origin[0] == "folder":
+			# plugin is installed via a plugin folder, need to use rmtree to get rid of it
+			import shutil
+			import os
+			shutil.rmtree(os.path.realpath(plugin.location))
+
+	def command_toggle(self, plugin):
+		if plugin.key == "pluginmanager":
+			return make_response("Can't enable/disable Plugin Manager", 400)
+
+		if plugin.enabled:
+			# disabled plugin
+			pass
+		else:
+			# enable plugin
+			pass
 
 
 __plugin_implementations__ = [PluginManagerPlugin()]
--- octoprint_pluginmanager/static/js/pluginmanager.js
+++ octoprint_pluginmanager/static/js/pluginmanager.js
@@ -23,6 +23,8 @@ $(function() {
             5
         );
 
+        self.installUrl = ko.observable();
+
         self.fromResponse = function(data) {
             self.plugins.updateItems(data.plugins);
         };
@@ -34,6 +36,59 @@ $(function() {
                 dataType: "json",
                 success: self.fromResponse
             });
+        };
+
+        self.togglePlugin = function(data) {
+            if (data.key == "pluginmanager") return;
+
+            var command = "toggle";
+            var payload = {plugin: data.key};
+            self._postCommand(command, payload, function() {self.requestData()});
+        };
+
+        self.installPlugin = function() {
+            var url = self.installUrl();
+            if (!url) return;
+
+            var command = "install";
+            var payload = {url: url};
+            self._postCommand(command, payload, function() {self.requestData()});
+        };
+
+        self.uninstallPlugin = function(data) {
+            if (data.bundled) return;
+            if (data.key == "pluginmanager") return;
+
+            var command = "uninstall";
+            var payload = {plugin: data.key};
+            self._postCommand(command, payload, function() {self.requestData()});
+        };
+
+        self._postCommand = function (command, data, successCallback, failureCallback, alwaysCallback, timeout) {
+            var payload = _.extend(data, {command: command});
+
+            var params = {
+                url: API_BASEURL + "plugin/pluginmanager",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify(payload),
+                contentType: "application/json; charset=UTF-8",
+                success: function(response) {
+                    if (successCallback) successCallback(response);
+                },
+                error: function() {
+                    if (failureCallback) failureCallback();
+                },
+                complete: function() {
+                    if (alwaysCallback) alwaysCallback();
+                }
+            };
+
+            if (timeout != undefined) {
+                params.timeout = timeout;
+            }
+
+            $.ajax(params);
         };
 
         self.onBeforeBinding = function() {
