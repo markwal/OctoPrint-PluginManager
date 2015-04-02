d6989510e1798d7eaec7f36f77af58eaa7214986
Gina Häußge <osd@foosel.net>
2015-04-02 23:02:41 +0200

    First throw at working plugin lifecycle management
    Plugins may be enabled and disabled during runtime. If they are of types which allow hot loading, this will be done. Otherwise they will be marked as pending and updated after a restart. Same for installation and uninstallation.

--- octoprint_pluginmanager/__init__.py
+++ octoprint_pluginmanager/__init__.py
@@ -7,13 +7,21 @@ __copyright__ = "Copyright (C) 2014 The OctoPrint Project - Released under terms
 
 
 import octoprint.plugin
+import octoprint.plugin.core
 
 from octoprint.settings import valid_boolean_trues
 from flask import jsonify, make_response
 
 class PluginManagerPlugin(octoprint.plugin.SimpleApiPlugin,
                           octoprint.plugin.TemplatePlugin,
-                          octoprint.plugin.AssetPlugin):
+                          octoprint.plugin.AssetPlugin,
+                          octoprint.plugin.SettingsPlugin):
+
+	def __init__(self):
+		self._pending_enable = set()
+		self._pending_disable = set()
+		self._pending_install = set()
+		self._pending_uninstall = set()
 
 	##~~ AssetPlugin
 
@@ -37,7 +45,8 @@ class PluginManagerPlugin(octoprint.plugin.SimpleApiPlugin,
 		return {
 			"install": ["url"],
 			"uninstall": ["plugin"],
-			"toggle": ["plugin"]
+			"enable": ["plugin"],
+			"disable": ["plugin"]
 		}
 
 	def on_api_get(self, request):
@@ -47,17 +56,7 @@ class PluginManagerPlugin(octoprint.plugin.SimpleApiPlugin,
 		result = []
 		for d in (plugins, disabled_plugins):
 			for name, plugin in d.items():
-				result.append(dict(
-					key=name,
-					name=plugin.name,
-					description=plugin.description,
-					author=plugin.author,
-					version=plugin.version,
-					url=plugin.url,
-					license=plugin.license,
-					bundled=plugin.bundled,
-					enabled=plugin.enabled
-				))
+				result.append(self._to_external_representation(plugin))
 
 		return jsonify(plugins=result)
 
@@ -68,25 +67,35 @@ class PluginManagerPlugin(octoprint.plugin.SimpleApiPlugin,
 
 		elif command == "uninstall":
 			plugin_name = data["plugin"]
-			if not plugin_name in self._plugin_manager.plugins:
+			if not plugin_name in self._plugin_manager.all_plugins:
 				return make_response("Unknown plugin: %s" % plugin_name, 404)
 
-			plugin = self._plugin_manager.plugins[plugin_name]
+			plugin = self._plugin_manager.all_plugins[plugin_name]
 			return self.command_uninstall(plugin)
 
-		elif command == "toggle":
+		elif command == "enable" or command == "disable":
 			plugin_name = data["plugin"]
-			if not plugin_name in self._plugin_manager.plugins:
+			if not plugin_name in self._plugin_manager.all_plugins:
 				return make_response("Unknown plugin: %s" % plugin_name, 404)
 
-			plugin = self._plugin_manager.plugins[plugin_name]
-			return self.command_toggle(plugin)
+			plugin = self._plugin_manager.all_plugins[plugin_name]
+			return self.command_toggle(plugin, command)
 
 	def command_install(self, url, force=False):
 		try:
 			import pip as _pip
 		except:
 			return make_response("Could not instantiate pip, can't install that, sorry", 500)
+
+		import pkg_resources
+
+		def working_set_callback(distribution):
+			self._logger.info("WOrking set changed: {!r}".format(distribution))
+		pkg_resources.working_set.subscribe(working_set_callback)
+
+		# TODO need to solve issue of users externally modifying plugin folder, which could lead to more than
+		# one plugin being found after installation of a package
+		all_plugins_before = self._plugin_manager.all_plugins
 
 		pip_args = ["install", url]
 		try:
@@ -103,19 +112,44 @@ class PluginManagerPlugin(octoprint.plugin.SimpleApiPlugin,
 					self._logger.exception("Could not install plugin from %s" % url)
 					return make_response("Could not install plugin from url, see the log for more details", 500)
 
+		self._plugin_manager.reload_plugins()
+		all_plugins_after = self._plugin_manager.all_plugins
+
+		new_plugins = set(all_plugins_after.keys()) - set(all_plugins_before.keys())
+		if len(new_plugins) == 0 or len(new_plugins) > 1:
+			# no new plugin or more than one new plugin? Something must have gone wrong...
+			return make_response("Installed plugin, but could not find it afterwards", 500)
+
+		new_plugin_key = new_plugins.pop()
+		new_plugin = all_plugins_after[new_plugin_key]
+
+		needs_restart = new_plugin.implementation and isinstance(new_plugin.implementation, octoprint.plugin.core.RestartNeedingPlugin)
+		needs_refresh = new_plugin.implementation and isinstance(new_plugin.implementation, octoprint.plugin.ReloadNeedingPlugin)
+
+		if not needs_restart:
+			try:
+				self._plugin_manager.enable_plugin(new_plugin_key, plugin=new_plugin)
+			except octoprint.plugin.core.PluginLifecycleException as e:
+				self._logger.exception("Problem enabling plugin {name}".format(name=new_plugin_key))
+				return jsonify(result=False, installed=True, enabled=False, reason=e.reason)
+
+		self._plugin_manager.log_all_plugins()
+
+		return jsonify(dict(result=True, needs_restart=needs_restart, needs_refresh=needs_refresh, plugin=self._to_external_representation(new_plugin)))
+
 	def command_uninstall(self, plugin):
 		if plugin.key == "pluginmanager":
 			return make_response("Can't uninstall Plugin Manager", 400)
-
-		try:
-			import pip as _pip
-		except:
-			return make_response("Could not instantiate pip, can't install that, sorry", 500)
 
 		if plugin.bundled:
 			return make_response("Bundled plugins cannot be uninstalled", 400)
 
 		if plugin.origin[0] == "entry_point":
+			try:
+				import pip as _pip
+			except:
+				return make_response("Could not instantiate pip, can't install that, sorry", 500)
+
 			# plugin is installed through entry point, need to use pip to uninstall it
 			pip_args = ["uninstall", "--yes", plugin.origin[2]]
 			_pip.main(pip_args)
@@ -126,16 +160,90 @@ class PluginManagerPlugin(octoprint.plugin.SimpleApiPlugin,
 			import os
 			shutil.rmtree(os.path.realpath(plugin.location))
 
-	def command_toggle(self, plugin):
+		needs_restart = plugin.implementation and isinstance(plugin.implementation, octoprint.plugin.core.RestartNeedingPlugin)
+		needs_refresh = plugin.implementation and isinstance(plugin.implementation, octoprint.plugin.ReloadNeedingPlugin)
+
+		if not needs_restart:
+			try:
+				self._plugin_manager.disable_plugin(plugin.key, plugin=plugin)
+			except octoprint.plugin.core.PluginLifecycleException as e:
+				self._logger.exception("Problem disabling plugin {name}".format(name=plugin.key))
+				return jsonify(result=False, uninstalled=True, disabled=False, unloaded=False, reason=e.reason)
+
+			try:
+				self._plugin_manager.unload_plugin(plugin.key)
+			except octoprint.plugin.core.PluginLifecycleException as e:
+				self._logger.exception("Problem unloading plugin {name}".format(name=plugin.key))
+				return jsonify(result=False, uninstalled=True, disabled=True, unloaded=False, reason=e.reason)
+
+		return jsonify(result=True, needs_restart=needs_restart, needs_refresh=needs_refresh, plugin=self._to_external_representation(plugin))
+
+	def command_toggle(self, plugin, command):
 		if plugin.key == "pluginmanager":
 			return make_response("Can't enable/disable Plugin Manager", 400)
 
-		if plugin.enabled:
-			# disabled plugin
-			pass
+		needs_restart = plugin.implementation and isinstance(plugin.implementation, octoprint.plugin.core.RestartNeedingPlugin)
+		needs_refresh = plugin.implementation and isinstance(plugin.implementation, octoprint.plugin.ReloadNeedingPlugin)
+
+		try:
+			if command == "disable":
+				self._mark_plugin_disabled(plugin, needs_restart=needs_restart)
+			elif command == "enable":
+				self._mark_plugin_enabled(plugin, needs_restart=needs_restart)
+		except octoprint.plugin.core.PluginLifecycleException as e:
+			self._logger.exception("Problem toggling enabled state of {name}: {reason}".format(name=plugin.key, reason=e.reason))
+			return jsonify(result=False, reason=e.reason)
+		except octoprint.plugin.core.RestartNeedingPlugin:
+			return jsonify(result=True, needs_restart=True, needs_refresh=True, plugin=self._to_external_representation(plugin))
 		else:
-			# enable plugin
-			pass
+			return jsonify(result=True, needs_restart=needs_restart, needs_refresh=needs_refresh, plugin=self._to_external_representation(plugin))
+
+	def _mark_plugin_enabled(self, plugin, needs_restart=False):
+		disabled_list = list(self._settings.global_get(["plugins", "_disabled"]))
+		if plugin.key in disabled_list:
+			disabled_list.remove(plugin.key)
+			self._settings.global_set(["plugins", "_disabled"], disabled_list)
+			self._settings.save(force=True)
+
+		if not needs_restart:
+			self._plugin_manager.enable_plugin(plugin.key)
+		else:
+			if plugin.key in self._pending_disable:
+				self._pending_disable.remove(plugin.key)
+			elif not plugin.enabled and plugin.key not in self._pending_enable:
+				self._pending_enable.add(plugin.key)
+
+	def _mark_plugin_disabled(self, plugin, needs_restart=False):
+		disabled_list = list(self._settings.global_get(["plugins", "_disabled"]))
+		if not plugin.key in disabled_list:
+			disabled_list.append(plugin.key)
+			self._settings.global_set(["plugins", "_disabled"], disabled_list)
+			self._settings.save(force=True)
+
+		if not needs_restart:
+			self._plugin_manager.disable_plugin(plugin.key)
+		else:
+			if plugin.key in self._pending_enable:
+				self._pending_enable.remove(plugin.key)
+			elif plugin.enabled and plugin.key not in self._pending_disable:
+				self._pending_disable.add(plugin.key)
+
+	def _to_external_representation(self, plugin):
+		return dict(
+			key=plugin.key,
+			name=plugin.name,
+			description=plugin.description,
+			author=plugin.author,
+			version=plugin.version,
+			url=plugin.url,
+			license=plugin.license,
+			bundled=plugin.bundled,
+			enabled=plugin.enabled,
+			pending_enable=(not plugin.enabled and plugin.key in self._pending_enable),
+			pending_disable=(plugin.enabled and plugin.key in self._pending_disable),
+			pending_install=(plugin.key in self._pending_install),
+			pending_uninstall=(plugin.key in self._pending_uninstall)
+		)
 
 __plugin_name__ = "Plugin Manager"
 __plugin_implementation__ = PluginManagerPlugin()
--- octoprint_pluginmanager/static/js/pluginmanager.js
+++ octoprint_pluginmanager/static/js/pluginmanager.js
@@ -41,27 +41,158 @@ $(function() {
         self.togglePlugin = function(data) {
             if (data.key == "pluginmanager") return;
 
-            var command = "toggle";
+            var titleSuccess, textSuccess, textRestart, textReload, titleError, textError;
+            var command;
+            if (!data.enabled || data.pending_disable) {
+                command = "enable";
+                titleSuccess = _.sprintf(gettext("Plugin %(name)s enabled"), {name: data.name});
+                textSuccess = gettext("The plugin was enabled successfully.");
+                textRestart = gettext("The plugin was enabled successfully, however a restart of OctoPrint is needed for that to take effect.");
+                textReload = gettext("The plugin was enabled successfully, however a reload of the page is needed for that to take effect.");
+            } else if (data.enabled || data.pending_enable) {
+                command = "disable";
+                titleSuccess = _.sprintf(gettext("Plugin %(name)s disabled"), {name: data.name});
+                textSuccess = gettext("The plugin was disabled successfully.");
+                textRestart = gettext("The plugin was disabled successfully, however a restart of OctoPrint is needed for that to take effect.");
+                textReload = gettext("The plugin was disabled successfully, however a reload of the page is needed for that to take effect.");
+            } else {
+                return;
+            }
+
+            titleError = gettext("Something went wrong");
+            textError = gettext("Toggling the plugin failed, please see the log for details.");
+            textErrorReason = gettext("Toggling the plugin failed: %(reason)s");
+
             var payload = {plugin: data.key};
-            self._postCommand(command, payload, function() {self.requestData()});
+            self._postCommand(command, payload, function(response) {
+                if (!response.result && response.hasOwnProperty("reason") && response.reason) {
+                    textError = _.sprintf(textErrorReason, {reason: response.reason});
+                }
+
+                self._displayNotification(response, titleSuccess, textSuccess, textRestart, textReload, titleError, textError);
+                self.requestData();
+            }, function() {
+                new PNotify({
+                    title: titleError,
+                    text: textError,
+                    type: "error",
+                    hide: false
+                })
+            });
         };
 
         self.installPlugin = function() {
             var url = self.installUrl();
             if (!url) return;
 
+            var titleSuccess, textSuccess, textRestart, textReload, titleError, textError, textErrorReason;
+            titleSuccess = gettext("Plugin %(name)s installed");
+            textSuccess = gettext("The plugin was installed successfully");
+            textRestart = gettext("The plugin was installed successfully, however a restart of OctoPrint is needed for that to take effect.");
+            textReload = gettext("The plugin was installed successfully, however a reload of the page is needed for that to take effect.");
+            titleError = gettext("Something went wrong");
+            textError = gettext("Installing the plugin from URL %(url)s failed, please see the log for details.");
+            textErrorReason = gettext("Installing the plugin from URL %(url)s failed: %(reason)s");
+
             var command = "install";
             var payload = {url: url};
-            self._postCommand(command, payload, function() {self.requestData()});
+            self._postCommand(command, payload, function(response) {
+                var name = (response.hasOwnProperty("plugin")) ? response.plugin.name : "Unknown";
+
+                if (!response.result && response.hasOwnProperty("reason") && response.reason) {
+                    textError = _.sprintf(textErrorReason, {url: url, reason: response.reason});
+                } else {
+                    textError = _.sprintf(textError, {url: url});
+                }
+
+                self._displayNotification(response, _.sprintf(titleSuccess, {name: name}), textSuccess, textRestart, textReload, titleError, textError);
+                self.requestData();
+            }, function() {
+                new PNotify({
+                    title: titleError,
+                    text: textError,
+                    type: "error",
+                    hide: false
+                })
+            });
         };
 
         self.uninstallPlugin = function(data) {
             if (data.bundled) return;
             if (data.key == "pluginmanager") return;
 
+            var titleSuccess, textSuccess, textRestart, textReload, titleError, textError, textErrorReason;
+
+            titleSuccess = _.sprintf(gettext("Plugin %(name)s uninstalled"), {name: data.name});
+            textSuccess = gettext("The plugin was uninstalled successfully");
+            textRestart = gettext("The plugin was uninstalled successfully, however a restart of OctoPrint is needed for that to take effect.");
+            textReload = gettext("The plugin was uninstalled successfully, however a reload of the page is needed for that to take effect.");
+            titleError = gettext("Something went wrong");
+            textError = gettext("Uninstalling the plugin failed, please see the log for details.");
+            textErrorReason = gettext("Uninstalling the plugin failed: %(reason)s");
+
             var command = "uninstall";
             var payload = {plugin: data.key};
-            self._postCommand(command, payload, function() {self.requestData()});
+            self._postCommand(command, payload, function(response) {
+                if (!response.result && response.hasOwnProperty("reason") && response.reason) {
+                    textError = _.sprintf(textErrorReason, {reason: response.reason});
+                }
+
+                self._displayNotification(response, titleSuccess, textSuccess, textRestart, textReload, titleError, textError);
+                self.requestData()
+            }, function() {
+                new PNotify({
+                    title: titleError,
+                    text: textError,
+                    type: "error",
+                    hide: false
+                })
+            });
+        };
+
+        self._displayNotification = function(response, titleSuccess, textSuccess, textRestart, textReload, titleError, textError) {
+            if (response.result) {
+                if (response.needs_restart) {
+                    new PNotify({
+                        title: titleSuccess,
+                        text: textRestart,
+                        hide: false
+                    });
+                } else if (response.needs_refresh) {
+                    new PNotify({
+                        title: titleSuccess,
+                        text: textReload,
+                        confirm: {
+                            confirm: true,
+                            buttons: [{
+                                text: gettext("Reload now"),
+                                click: function () {
+                                    location.reload(true);
+                                }
+                            }]
+                        },
+                        buttons: {
+                            closer: false,
+                            sticker: false
+                        },
+                        hide: false
+                    })
+                } else {
+                    new PNotify({
+                        title: titleSuccess,
+                        text: textSuccess,
+                        type: "success",
+                        hide: false
+                    })
+                }
+            } else {
+                new PNotify({
+                    title: gettext("Something went wrong"),
+                    text: gettext("Toggling the plugin failed, please see the log for details"),
+                    type: "error",
+                    hide: false
+                });
+            }
         };
 
         self._postCommand = function (command, data, successCallback, failureCallback, alwaysCallback, timeout) {
@@ -91,6 +222,10 @@ $(function() {
             $.ajax(params);
         };
 
+        self.toggleButtonTitle = function(data) {
+            return (!data.enabled || data.pending_disable) ? gettext("Enable Plugin") : gettext("Disable Plugin");
+        };
+
         self.onBeforeBinding = function() {
             self.settings = self.settingsViewModel.settings;
             self.requestData();
@@ -102,5 +237,5 @@ $(function() {
     }
 
     // view model class, parameters for constructor, container to bind to
-    ADDITIONAL_VIEWMODELS.push([PluginManagerViewModel, ["loginStateViewModel", "settingsViewModel"], document.getElementById("settings_plugin_pluginmanager")]);
+    ADDITIONAL_VIEWMODELS.push([PluginManagerViewModel, ["loginStateViewModel", "settingsViewModel"], "#settings_plugin_pluginmanager"]);
 });
